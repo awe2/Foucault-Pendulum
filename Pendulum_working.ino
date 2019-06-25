a94ddc9de950e412adb8ec2d5346b5f584faca76
awe2 <39313529+awe2@users.noreply.github.com>
2019-06-24 19:24:37 -0500

    Add files via upload

---
+++ Pendulum_working.ino
@@ -0,0 +1,25 @@
+#define HALL_EFFECT 6
+#define RELAY 3
+
+int hallValue = 0;
+
+void setup() {
+  pinMode(HALL_EFFECT, INPUT);
+  pinMode(RELAY, OUTPUT);
+  digitalWrite(RELAY, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  hallValue = digitalRead(HALL_EFFECT);
+  Serial.println(hallValue);
+
+  if(hallValue == LOW){
+    delay(50); //delay for wait till past center
+    digitalWrite(RELAY, HIGH);
+    delay(500); //delay to keep pendulem on
+    digitalWrite(RELAY, LOW);
+    delay(500); //delay to turn hall effect off
+  }
+  //digitalWrite(RELAY, HIGH);
+}
