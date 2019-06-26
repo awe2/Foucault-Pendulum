9f813fcb4bea7651072007fd2c57c0dc1bc67401
awe2 <39313529+awe2@users.noreply.github.com>
2019-06-25 21:56:34 -0500

    Add files via upload

--- Pendulum_working.ino
+++ Pendulum_working.ino
@@ -1,6 +1,8 @@
 #define HALL_EFFECT 6
 #define RELAY 3
 
+int LEDPIN = 13;
+
 int hallValue = 0;
 
 void setup() {
@@ -8,6 +10,9 @@ void setup() {
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, LOW);
   Serial.begin(9600);
+  {
+  pinMode(LEDPIN, OUTPUT);
+} 
 }
 
 void loop() {
@@ -17,9 +22,12 @@ void loop() {
   if(hallValue == LOW){
     delay(50); //delay for wait till past center
     digitalWrite(RELAY, HIGH);
+    digitalWrite(LEDPIN, HIGH);
     delay(500); //delay to keep pendulem on
     digitalWrite(RELAY, LOW);
-    delay(500); //delay to turn hall effect off
+    digitalWrite(LEDPIN,LOW);
+    //delay(500); //delay to turn hall effect off
+    delay(14000); //delay to turn hall effect off, and keep off for a long time because we want the pendulum to precess (delays about 5 passes)
   }
   //digitalWrite(RELAY, HIGH);
 }
